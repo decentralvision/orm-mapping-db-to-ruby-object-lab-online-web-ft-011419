52b11a1b139215e7913b1cdf2481617ff78a8d7a
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-16 20:01:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,7 @@ class Student
     # create a new Student object given a row from the database
     student = self.new
     student.id, student.name, student.grade = row[0], row[1], row[2]
+    student
   end
 
   def self.all
