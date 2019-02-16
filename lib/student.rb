51b70e9c65a7467c5fb1081cfafacee85a68c3ba
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-16 20:27:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
     DB[:conn].execute("SELECT * FROM students WHERE grade < '12'").collect {|row| self.new_from_db(row)}
   end
   def self.first_X_students_in_grade_10(num)
-    DB[:conn].execute("SELECT * FROM students WHERE grade = '10' LIMIT ?", num)
+    DB[:conn].execute("SELECT * FROM students WHERE grade = '10' LIMIT ?", num).collect {|row| self.new_from_db(row)}
   end
 
   def self.drop_table
