1596d6e5aca2a745a253ca3c7095748251e08e95
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-16 20:28:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
     DB[:conn].execute("SELECT * FROM students WHERE grade = '10' LIMIT ?", num).collect {|row| self.new_from_db(row)}
   end
   def self.first_student_in_grade_10
-    DB[:conn].execute("SELECT * FROM students WHERE grade = '10' LIMIT 1", num).collect {|row| self.new_from_db(row)}
+     self.new_from_db(DB[:conn].execute("SELECT * FROM students WHERE grade = '10' LIMIT 1", num))
   end
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
