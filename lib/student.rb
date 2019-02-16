34a025d596bc94d08bd820255da528b18ce2928d
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-16 20:17:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,8 +47,7 @@ class Student
   end
 
   def self.students_below_12th_grade
-    DB[:conn].execute("SELECT * FROM students WHERE grade < '12'").each {|student| self.new_from_db(student)}
-    binding.pry
+    DB[:conn].execute("SELECT * FROM students WHERE grade < '12'").each {|row| self.new_from_db(row)}
   end
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
